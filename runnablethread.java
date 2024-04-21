68e3dd196e73d339e7cc3696ca1086d0c8976b7f
Sourav Paul <145861080+Sourav4x@users.noreply.github.com>
2024-04-21 08:04:17 +0530

    Add files via upload

---
+++ runnablethread.java
@@ -0,0 +1,45 @@
+import java.util.*;
+class A implements Runnable{
+    public void run()
+    {
+        for(int i=0;i<1000;i++);
+        {
+
+        
+        System.out.println("IN A CLASS");
+    }
+}
+}
+class B implements Runnable{
+    public void run()
+    {
+        for(int i=0;i<1000;i++);           // Run this prog multiple times without try catch block below to have diff output
+        {
+        System.out.println("IN B Class");
+        }
+    }
+}
+
+class runnablethread{
+    public static void main(String[] args)
+    {
+        Runnable obj1=new A();
+        Runnable obj2=new B();
+
+        Thread t1=new Thread(obj1);
+        Thread t2=new Thread(obj2);
+
+       System.out.println(t1.getPriority());  // provides priority as the method suggests 
+        t1.setPriority(Thread.MAX_PRIORITY-2);
+         System.out.println(t1.getPriority());
+     
+        t1.start();
+        try{
+            Thread.sleep(20);
+        }catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        t2.start();
+    }
+}
